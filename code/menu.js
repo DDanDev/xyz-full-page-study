393e872986f10ded25c46c087ac7806c51275a4e
DDanDev <110492441+DDanDev@users.noreply.github.com>
2022-11-03 10:20:46 -0300

    links github pages fix?

--- code/menu.js
+++ code/menu.js
@@ -5,12 +5,12 @@ menu.innerHTML = `<div class="thebank" id="thebank">
 <img src="./assets/hamburgermenu.png" id="hamburger" />
 </div>
 <nav id="navMenu" class="mobileHidden">
-<a href="/index.html" id="/index.html">Home</a>
-<a href="/sobre.html" id="/sobre.html">Sobre</a>
-<a href="/contato.html" id="/contato.html">Contato</a>
-<a href="/blog.html" id="/blog.html">Blog</a>
-<a href="/carreira.html" id="/carreira.html">Carreira</a>
-<a href="/cadastrese.html" id="/cadastrese.html" class="cadastrese">Cadastre-se</a>
+<a href="./index.html" id="/index.html">Home</a>
+<a href="./sobre.html" id="/sobre.html">Sobre</a>
+<a href="./contato.html" id="/contato.html">Contato</a>
+<a href="./blog.html" id="/blog.html">Blog</a>
+<a href="./carreira.html" id="/carreira.html">Carreira</a>
+<a href="./cadastrese.html" id="/cadastrese.html" class="cadastrese">Cadastre-se</a>
 </nav>
 <link rel="stylesheet" href="./code/menu.css" />
 `;
